9bc55288a928e086190664d2cafc5bb06a1971e6
MatiasCanevaro <mcanevaro@frba.utn.edu.ar>
2024-12-29 19:49:01 -0300

    refactor itemController route

--- routes/router.js
+++ routes/router.js
@@ -4,7 +4,7 @@ const itemController = require("../controllers/itemController");
 const router = Router();
 
 router.get("/", homeController.get);
-router.get("/item/:categoryId", itemController.getAllCategoryItems);
+router.get("/category/:categoryId/items", itemController.getAllCategoryItems);
 
 
 module.exports = router;
