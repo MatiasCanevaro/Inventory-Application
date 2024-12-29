1795498df56154f3298491fbd857c30f5d9b3692
MatiasCanevaro <mcanevaro@frba.utn.edu.ar>
2024-12-29 18:48:27 -0300

    fix seed.js

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
-
 require("dotenv").config();
 const express = require("express");
 const app = express();
 const router = require("./routes/router");
-const populatedb = require("./db/populatedb");
  
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use("/", router);
-
-populatedb.main();
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Express app listening on port ${PORT}!`));
--- db/pool.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require("pg");
-
-/* module.exports = new Pool({
-    connectionString: "postgresql://postgres:123456@localhost:5432/top_users"
-}); */
-
-module.exports = new Pool({
-    connectionString: process.env.DATABASE_URL
-});
